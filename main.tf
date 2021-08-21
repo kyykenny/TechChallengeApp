4f36e214fa6c04118f45f4de739dbc3addd1e97a
Kenny kan <kennykan@Kennys-MacBook-Pro.local>
2021-08-21 23:48:54 +1000

    Remove route table

--- main.tf
+++ main.tf
@@ -40,31 +40,6 @@ resource "aws_subnet" "public-subnet-2" {
   }
 }
 
-resource "aws_security_group" "public-sg" {
-  name        = "public-SG"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.default-vpc.id
-
-  ingress {
-    description = "HTTP from VPC"
-    from_poroutetable   = 80
-    to_poroutetable     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_poroutetable   = 0
-    to_poroutetable     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "public-SG"
-  }
-}
-
 resource "aws_ecr_repository" "servian-app-ecr-repo" {
   name = "servian-app-ecr-repo" 
 }
