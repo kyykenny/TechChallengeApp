e768db7e68ea2d36f7de75d9eb13270c950421a1
Kenny kan <kennykan@Kennys-MacBook-Pro.local>
2021-08-22 20:29:59 +1000

    Add IGW for ALB

--- main.tf
+++ main.tf
@@ -96,6 +96,37 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.default-vpc.id
+
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.default-vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
 resource "aws_alb" "application_load_balancer" {
   name               = "servian-app-lb"
   load_balancer_type = "application"
