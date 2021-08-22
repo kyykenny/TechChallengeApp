b800ab9e7b0bbc2f3cbcc19f928f5c1ef0e660bd
Kenny kan <kennykan@Kennys-MacBook-Pro.local>
2021-08-22 20:11:04 +1000

    Modify main.tf and deploy pipeline

--- main.tf
+++ main.tf
@@ -48,3 +48,131 @@ resource "aws_ecr_repository" "servian-app-ecr-repo" {
 resource "aws_ecs_cluster" "servian-app-cluster" {
   name = "servian-app-cluster"
 }
+
+resource "aws_ecs_task_definition" "servian-app-task" {
+  family                   = "servian-app-task" 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "servian-app-task",
+      "image": "${aws_ecr_repository.servian-app-ecr-repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] 
+  network_mode             = "awsvpc"    
+  memory                   = 512        
+  cpu                      = 256        
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "servian-app-lb"
+  load_balancer_type = "application"
+  subnets = [ 
+    "${aws_subnet.public-subnet-1.id}",
+    "${aws_subnet.public-subnet-2.id}"
+  ]
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_vpc.default-vpc.id}" 
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" 
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
+}
+
+resource "aws_ecs_service" "servian-app-service" {
+  name            = "servian-app-service"
+  cluster         = "${aws_ecs_cluster.servian-app-cluster.id}"
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.servian-app-task.family}"
+    container_port   = 3000 
+  }
+
+  network_configuration {
+    subnets          = ["${aws_subnet.public-subnet-1.id}", "${aws_subnet.public-subnet-1.id}"]
+    assign_public_ip = true                                                
+    security_groups  = ["${aws_security_group.service_security_group.id}"]
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
